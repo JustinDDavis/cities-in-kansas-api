2c916d0de497cceaaeb05e6c936cd344a9d03408
Justin Davis <justindaniel92@gmail.com>
2020-01-30 19:16:42 -0600

    Fixed start file so flask and run from the root

--- app/app.py
+++ app/app.py
@@ -3,10 +3,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Home"
+
 @app.route('/api/ks', methods=['GET'])
 def get_cities():
     data = "Data to be implemented"
-    # Prepare data that may have been filtered
     results = {
         'data' : data
     } 
