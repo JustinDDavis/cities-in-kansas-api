dc3c3c2883de02bac92f24248c7c5ad383a86e40
Justin Davis <justindaniel92@gmail.com>
2020-01-30 20:10:18 -0600

    Adding full path

--- app/data_utilities.py
+++ app/data_utilities.py
@@ -1,5 +1,10 @@
+import os
+cwd = os.getcwd()
+
 def load_data_file(name):
-    with open('data/ks_cities.txt', 'r') as file:
+    print("Data file")
+    print(cwd+'/data/ks_cities.txt')
+    with open(cwd+'/data/ks_cities.txt', 'r') as file:
         data = []
         for file_line in file:
             data.append(file_line.rstrip())
